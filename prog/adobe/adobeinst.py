f829717617f3861c8b1a987481d52dd1f765a0c6
Bryan <bcking284@gmail.com>
2019-12-12 15:29:19 -0600

    added images, fixed adobe

--- prog/adobe/adobeinst.py
+++ prog/adobe/adobeinst.py
@@ -4,3 +4,7 @@ from clicks import *
 
 def install_adobe():
     double_click_image('prog/adobe/imgs/installertooltip.png')
+    single_click_image('prog/adobe/imgs/launch_checkbox.png')
+    double_click_image('prog/adobe/imgs/finish_button.png')
+    right_click_image('prog/adobe/imgs/chrome_taskbar.png')
+    single_click_image('prog/adobe/imgs/close_button.png')
